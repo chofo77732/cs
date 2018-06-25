6b28a6c31ac10ebc473963db7aa0ded851e1bfa2
chofo77732 <30615047+chofo77732@users.noreply.github.com>
2018-06-24 22:14:36 -0500

    actualización
    Actualización
    enemigo e item

---
+++ 2d_mundo1/Assets/item.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class item : MonoBehaviour {
+
+
+	public GameObject mensaje;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	
+}
--- 2d_mundo1/Assets/player.cs
+++ 2d_mundo1/Assets/player.cs
@@ -5,32 +5,81 @@ using UnityEngine;
 public class player : MonoBehaviour {
 
 	public float speed = 4f;
+	public GameObject mensaje;
+
 
 	Animator anim;
+	Rigidbody2D rb2d;
+	Vector2 mov;  // Ahora es visible entre los métodos
+	Vector2 inicio;  // Ahora es visible entre los métodos
+
 
 	void Start () {
-  
 		anim = GetComponent<Animator>();
-    
+		rb2d = GetComponent<Rigidbody2D>();
+		inicio = transform.position;
 	}
 
 	void Update () {
 
-		Vector3 mov = new Vector3(
+		if(Input.GetKeyDown("space")){
+			if(Time.timeScale == 1){   
+		} else if(Time.timeScale == 0) {   
+			 Time.timeScale = 1;  
+			 mensaje.SetActive(false);
+		}
+
+		}
+		// Detectamos el movimiento en un vector 2D
+		mov = new Vector2(
 			Input.GetAxisRaw("Horizontal"),
-			Input.GetAxisRaw("Vertical"),
-			0
+			Input.GetAxisRaw("Vertical")
 		);
 
-		transform.position = Vector3.MoveTowards(
-			transform.position, 
-			transform.position + mov, 
-			speed * Time.deltaTime
-		);
-
-		anim.SetFloat("movX", mov.x);
-		anim.SetFloat("movY", mov.y);
+		// Establecemos las animaciones
+		if (mov != Vector2.zero) {
+			anim.SetFloat("movX", mov.x);
+			anim.SetFloat("movY", mov.y);
+			anim.SetBool("walking", true);
+		} else {
+			anim.SetBool("walking", false);
+		}
 
 	}
-  
+
+	void FixedUpdate () {
+		// Nos movemos en el fixed por las físicas
+		rb2d.MovePosition(rb2d.position + mov * speed * Time.deltaTime);
+	}
+
+
+		void OnTriggerEnter2D(Collider2D col){
+	
+		if(col.gameObject.tag == "prueba"){
+			
+			col.gameObject.SetActive(false);
+			//mensaje.SetActive(true);
+			//StartCoroutine(espera());
+			Debug.Log ("toco el objeto");
+			//mensaje.SetActive(false);
+
+			Time.timeScale = 0;
+			mensaje.SetActive(true);
+
+			
+		}else if(col.gameObject.tag == "enemigo"){
+			
+			Debug.Log ("toco al enemigo");
+			transform.position = new Vector2(0,0);
+			//mensaje.SetActive(false);
+			
+		}
+	}
+
+	IEnumerator espera()
+    {
+    	yield return new WaitForSeconds(3);
+        mensaje.SetActive(false);
+
+    }
 }
